339ff5833053129ef25334e68326195dde5824b0
icebergmountainjaguar9-dev <icebergmountainjaguar9@gmail.com>
2026-05-31 06:19:19 +0000

    new edits

--- sushitoken.js
+++ sushitoken.js
@@ -2,3 +2,20 @@ document.getElementById("mineSushi").addEventListener("click", () => {
     document.getElementById("status").innerHTML =
         "🍣 SushiToken mining started...";
 });
+const startBtn = document.getElementById("startMiningBtn");
+const walletInput = document.getElementById("wallet");
+const statusBox = document.getElementById("status");
+
+startBtn.addEventListener("click", () => {
+
+    const wallet = walletInput.value.trim();
+
+    if(wallet.length < 20){
+        statusBox.innerHTML =
+            "Please enter a valid SushiToken Polygon address.";
+        return;
+    }
+
+    statusBox.innerHTML =
+        "🍣 Mining started for wallet:<br>" + wallet;
+});
